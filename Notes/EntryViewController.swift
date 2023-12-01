bcc14072961588f36740301a0c5e1267ef9e24ba
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-12-02 00:28:01 +0300

    First version of notes app is added.

---
+++ Notes/EntryViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EntryViewController.swift
+//  Notes
+//
+//  Created by ahmet berat can on 1.12.2023.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+    
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+        
+    }
+    
+    @objc func didTapSave() {
+        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
+            completion?(text, noteField.text)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
